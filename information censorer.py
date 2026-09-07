b8261b67aae54c8b5684fec958947560f99c094a
przemyslawszwarc01-ux <przemyslaw.szwarc01@gmail.com>
2026-09-07 13:03:29 +0200

    new

--- information censorer.py
+++ information censorer.py
@@ -1,18 +1,38 @@
 #just a script that censors a data
     #name
 name = input("Enter your name: ")
-Surname = input("Enter your surname: ")
+surname = input("Enter your surname: ")
     #birthdate
-print("whats your name?")
+print("whats your birth date?")
 dayd = input("day:")
 monthd = input("month:")
 year = input("year:")
     #info
 phone_num = input("Enter your Phone number: ")
 credit_card_num = input("Enter your credit card number: ")
-card_type =input("Enter your card type: ")
+#card_type =input("Enter your card type: ")
 
-for x in phone_num:
-    print(x)
+#First name
+nameLenght = len(name)
+cnameLenght = int(nameLenght-1)
+print(name[0:1],"X"*cnameLenght)
+
+#surname
+snamelenght = len(surname)
+csnamelenght = int(snamelenght-1)
+print(surname[0:1],"X"*csnamelenght)
+
+#date of birth
+print("XX-XX-",int(year))
+
+phonenumLenght = len(phone_num)
+cphoneNumLenght = int(phonenumLenght - 3)
+print("X"*int(cphoneNumLenght),int(phone_num[-3:]))
+
+ccnumLenght = len(credit_card_num)
+cccnumlenght = int(ccnumLenght - 4)
+print("X"*cccnumlenght,int(credit_card_num[-4:]))
+
+
 
 
